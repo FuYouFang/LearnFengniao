73f0a975a2c6eafed01faa61d135af94f71c54d7
fuyoufang <fuyoufang@163.com>
2017-03-20 13:20:39 +0800

    create base progect

--- Package.swift
+++ Package.swift
@@ -1,5 +1,18 @@
 import PackageDescription
 
+// package 的具体写法
+// https://github.com/apple/swift-package-manager
+
 let package = Package(
-    name: "LearnFengniao"
+    name: "LearnFengniao",
+    
+    // 添加依赖
+    // pre 的版本只能制定具体的版本
+    // majorVersion: 所有 2 的版本
+    // minorVersion: 最小的版本
+    dependencies: [
+        .Package(url: "https://github.com/jatoben/CommandLine", "3.0.0-pre1"), // pre
+        .Package(url: "https://github.com/onevcat/Rainbow", majorVersion: 2),
+        //.Package(url: "https://github.com/onevcat/Rainbow", minorVersion: 1)
+    ]
 )
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,68 @@
-print("Hello, world!")
+import Foundation
+import CommandLineKit
+import Rainbow
+
+//print("Hello, world!")
+
+// 打印 arguments
+//print(CommandLine.arguments)
+
+
+let cli = CommandLineKit.CommandLine()
+
+let projectOption = StringOption(shortFlag: "p", longFlag: "project", required: true,
+                                 helpMessage: "Path to the project.")
+let excludePathsOption = MultiStringOption(shortFlag: "e", longFlag: "exclude", helpMessage: "Excluded paths which should not search in.")
+let resourceExtensionsOption = MultiStringOption(shortFlag: "r", longFlag: "resourrce-extension",
+                          helpMessage: "Extension To search.")
+let fileExtensionsOption = MultiStringOption(shortFlag: "f", longFlag: "file-extensions",
+                      helpMessage: "File extensions to search with.")
+let help = BoolOption(shortFlag: "h", longFlag: "help",
+                              helpMessage: "Print a help message.")
+
+cli.addOptions(projectOption, excludePathsOption, resourceExtensionsOption, fileExtensionsOption, help)
+
+
+cli.formatOutput = { s, type in
+    var str: String
+    switch(type) {
+    case .error:
+        str = s.red.bold
+    case .optionFlag:
+        str = s.green.underline
+    case .optionHelp:
+        str = s.lightBlue
+    default:
+        str = s
+    }
+    
+    return cli.defaultFormat(s: str, type: type)
+}
+
+
+do {
+    try cli.parse()
+} catch {
+    cli.printUsage(error)
+    exit(EX_USAGE)
+}
+
+if (help.value) {
+    cli.printUsage();
+}
+
+//print(projectOption.value);
+
+
+let project = projectOption.value ?? "."
+let excludePaths = excludePathsOption.value ?? []
+let resourceExtensions = resourceExtensionsOption.value ?? ["png", "jpg", "imageset"]
+let fileExtensions = fileExtensionsOption.value ?? ["swift", "m", "mm", "xib", "storyboard"]
+
+
+
+
+
+
+
+
